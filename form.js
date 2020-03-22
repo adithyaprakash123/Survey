1fbccdf8eff5c9b1f257827e0ddb79d614b8d8b2
adithyaprakash123 <55025043+adithyaprakash123@users.noreply.github.com>
2020-03-22 13:43:51 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,47 @@
+class Form {
+
+    constructor() {
+        this.input = createInput("Name");
+        this.email = createInput("email")
+        this.button = createButton('Submit');
+        this.greeting = createElement('h2');
+        this.text = createElement('h1', "Lets Do A Survey On Our School Students");
+        this.question1 = createElement('h3', "Q1. Do you think we need to contribute free education for poor children upto 9th grade?");
+        this.radio = createRadio('h3');
+        this.radio.option('yes');
+        this.radio.option('No');
+        this.question2 = createElement('h3', "Q2. Would you be willing to contribute your old textbooks, old uniforms etc for these poor children?");
+        this.radio1 = createRadio('h3');
+        this.radio1.option('yes');
+        this.radio1.option('No');
+        this.question3 = createElement('h3', "Do you think this is a good survey?");
+        this.radio2 = createRadio('h3');
+        this.radio2.option('Yeah, ofcourse');
+        this.radio2.option('i am very dissapointed');
+    }
+
+
+    display() {
+        this.input.position(670, 255)
+        this.email.position(670, 165)
+        this.button.position(700, 700)
+        this.greeting.position(600, 105)
+        this.text.position(600, 5)
+        this.question1.position(500, 360)
+        this.radio.position(600, 420)
+        this.question2.position(500, 450)
+        this.radio1.position(600, 510)
+        this.question3.position(500, 540)
+        this.radio2.position(600, 600)
+
+        this.button.mousePressed(() => {
+            User.input = this.input.value();
+            User.email = this.email.value();
+            peopleCount += 1;
+            User.index = peopleCount;
+            User.update();
+            User.updateCount(peopleCount);
+        });
+
+    }
+}
